0106f090cc2956ac328707d58b8c8ee9b4ec27c7
Justin Riley <20hz20khz@gmail.com>
2023-10-09 09:55:52 -0400

    Create Recommendation.java

---
+++ Recommendation.java
@@ -0,0 +1,22 @@
+public class Recommendation
+{
+
+  /* Instance variable declarations go here, before constructors */
+  // STEP 1 of 4: Add one private instance variable
+
+
+  /* No Argument Constructor - same name as Class, no return type */
+  public Recommendation()
+  {
+      //  STEP 2 of 4: Set a default value for your instance variable
+      
+  }
+
+  /* Method definitions go here, after constructors */
+  //  STEP 3 of 4: Add one accessor method (getter) for your instance variable
+
+  //  STEP 4 of 4: Add one mutator method (setter) for your instance variable
+
+  
+
+}
