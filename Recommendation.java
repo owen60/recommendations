3cb9cf275db490be1bfd50d9e3c0186a550290cf
JacksonV <63075844+XiLx@users.noreply.github.com>
2023-10-09 10:13:23 -0400

    Update Recommendation.java
    Added description

--- Recommendation.java
+++ Recommendation.java
@@ -3,20 +3,26 @@ public class Recommendation
 
   /* Instance variable declarations go here, before constructors */
   // STEP 1 of 4: Add one private instance variable
+  private String description;
 
 
   /* No Argument Constructor - same name as Class, no return type */
   public Recommendation()
   {
       //  STEP 2 of 4: Set a default value for your instance variable
-      
+      description = "This is a defult description";
   }
 
   /* Method definitions go here, after constructors */
   //  STEP 3 of 4: Add one accessor method (getter) for your instance variable
+  public String getDesc() {
+    return description;
+  }
 
   //  STEP 4 of 4: Add one mutator method (setter) for your instance variable
-
+  public void setDesc(String _description) {
+    description = _description;
+  }
   
 
 }
